49efd5d2d4597cd06439eb0054c850cd17d07bc2
Rosadebruin <Rosa.de.Bruin@student.gildeopleidingen.nl>
2026-06-04 10:13:51 +0200

    Create top6.js

---
+++ Brons/top6.js
@@ -0,0 +1,13 @@
+// Brons opdracht 1: 
+const songs = ["Bohemian Rhapsody", "Starman", "Lies in the eyes of love", "When i'm small", "Be like a woman" ];
+
+// Brons opdracht 2:
+console.log(songs[0]);
+
+// Brons opdracht 3:
+console.log(songs[2]);
+
+// Brons opdracht 4:
+songs.forEach(function(song) {
+  console.log(song);
+});
